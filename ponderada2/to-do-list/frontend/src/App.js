656e1b41e656e10feda14335a90c6261124e76ec
edu-porto <eduardo.porto@sou.inteli.edu.br>
2023-08-23 19:54:39 -0300

    FEAT Adicionei o botão de add tasks e delete
    Tô com algum erro que não sei oq é, mas não consigo deletar todas as tasks e somar direito a quantia.
    Preciso checar

--- ponderada2/to-do-list/frontend/src/App.js
+++ ponderada2/to-do-list/frontend/src/App.js
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./components/Todo";
 import FilterButton from "./components/FilterButton"
 import Form from "./components/Form"
+import { nanoid } from "nanoid";
 
-function App(props) {
-  const taskList = props.tasks.map((task) => (<Todo 
+
+
+function App(props){
+  const [tasks, setTasks] = useState(props.tasks);
+
+  // Função que checa se uma tarefa foi completada ou não 
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // Checa se o id que foi clicado é igual ao id da task 
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);  
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+    }
+
+  const taskList = tasks.map((task) => (<Todo 
   id={task.id}
   name={task.name} 
   completed={task.completed}
   key={task.id}
+  toggleTaskCompleted={toggleTaskCompleted}
+  deleteTask={deleteTask}
+
+
    />));
+
+
+  // Nesta função que fica salvo os dados da minha tarefa 
+  function addTask(name){
+    // Essa lib incrementa 1 na id de cada task
+    const newTask = { id:`todo-${nanoid()}` , name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  // Conta quantas tarefas ainda tenho pendentes e define se é plural ou não 
+  const tasksNoun = taskList.length !== 1 ? "tarefas" : "tarefa";
+  const headingText = `${taskList.length} ${tasksNoun} pendentes`;
+  
+
+
   return (
     <div className="todoapp stack-large">
       <h1>TO-DO-LIST</h1>
-      <Form/>
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
     <FilterButton/>
     <FilterButton/>
     <FilterButton/>
 
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
