7fd4dd31380301324ddddf38ff7b3eddc06089f9
edu-porto <99202389+edu-porto@users.noreply.github.com>
2023-10-04 12:22:50 -0300

    small fix

--- ponderada4/frontend/src/App.js
+++ ponderada4/frontend/src/App.js
@@ -532,10 +532,8 @@ function MyForm() {
       
       const response = await axios.post('http://44.204.85.74/prediction/', postData);
       document.getElementById('api-response').innerHTML = `Visualizações mensais: ${JSON.stringify(response.data['prediction'])}`;
-      // Handle the response or any other logic here
       console.log('Data posted:', response.data);
     } catch (error) {
-      // Handle errors
       console.error('Error:', error);
     }
   };
